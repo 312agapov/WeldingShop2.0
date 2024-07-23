ccf249aa782ed762dfcb47dbb78e832b70ece4d2
LEONID <312agapov@gmai.com>
2024-07-23 12:13:34 +0400

    Добавил @Controller для отображения начальной страницы index.html

--- src/main/java/mazdarunner/weldingMagazine/Weld/WeldController.java
+++ src/main/java/mazdarunner/weldingMagazine/Weld/WeldController.java
@@ -1,43 +1,13 @@
 package mazdarunner.weldingMagazine.Weld;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.UUID;
-
-@RestController
-@RequestMapping(path = "/weld")
+@Controller
 public class WeldController {
-
-    private final WeldService weldService;
-
-    @Autowired
-    public WeldController(WeldService weldService) { //зачем чел создал конструктор?
-        this.weldService = weldService;
+    @GetMapping("/")
+    public String weldings(){
+        return "index";
     }
-
-    @GetMapping("/showallwelds")
-    public List<Weld> showAllWelds(){
-        return weldService.showAllWelds();
-    }
-
-    @PostMapping("/addweld")
-    public void addWeld(@RequestBody Weld weld){
-        weldService.addWeld(weld);
-    }
-
-    @DeleteMapping(path = "/delete/{weldId}")
-    public void deleteWeldByID(@PathVariable("weldId") UUID weldId){
-        weldService.deleteWeldByID(weldId);
-    }
-
-    @PutMapping(path = "/edit/{weldId}")
-    public void editWeldByID(@PathVariable("weldId") UUID weldId,
-                             @RequestParam(required = false) String name,
-                             @RequestParam(required = false) Integer price,
-                             @RequestParam(required = false) Integer maxPower){
-        weldService.updateWeldByID(weldId, name, price, maxPower);
-    }
-
 }
---
+++ src/main/java/mazdarunner/weldingMagazine/Weld/WeldRestController.java
@@ -0,0 +1,44 @@
+package mazdarunner.weldingMagazine.Weld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/weld")
+public class WeldRestController {
+
+    private final WeldService weldService;
+
+    @Autowired
+    public WeldRestController(WeldService weldService) { //зачем чел создал конструктор?
+        this.weldService = weldService;
+    }
+
+    @GetMapping("/showallwelds")
+    public List<Weld> showAllWelds(){
+        return weldService.showAllWelds();
+    }
+
+    @PostMapping("/addweld")
+    public void addWeld(@RequestBody Weld weld){
+        weldService.addWeld(weld);
+    }
+
+    @DeleteMapping(path = "/delete/{weldId}")
+    public void deleteWeldByID(@PathVariable("weldId") UUID weldId){
+        weldService.deleteWeldByID(weldId);
+    }
+
+    @PutMapping(path = "/edit/{weldId}")
+    public void editWeldByID(@PathVariable("weldId") UUID weldId,
+                             @RequestParam(required = false) String name,
+                             @RequestParam(required = false) Integer price,
+                             @RequestParam(required = false) Integer maxPower){
+        weldService.updateWeldByID(weldId, name, price, maxPower);
+    }
+
+}
